a849fd8e76a6270f91de635f2760200c70f53137
Asep Ready <savalunova@gmail.com>
2025-01-04 14:40:04 +0700

    break line

--- root/bootstrap.sh
+++ root/bootstrap.sh
@@ -5,8 +5,7 @@ cd /root/project; chown -R www:www database; chown -R www:www storage
 #cd /root/project; chmod 644 .env; chown www:www .env
 #cd /root/project; php artisan migrate
 #cd /root/; php artisan serve --host 0.0.0.0 --port=80
-
-rm /root/bootstrap.sh
-
 cd /root; curl -X PUT --data-binary @config.json --unix-socket \
     /var/run/unit/control.unit.sock http://localhost/config/
+
+rm /root/bootstrap.sh
